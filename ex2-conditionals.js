89821c85f1df7b5bcb7ffa1d3ed388aca1fd982e
Kamushabe Ampulira <kamushabeampulira@gmail.com>
2026-03-23 10:28:40 +0300

    added new file

---
+++ ex2-conditionals.js
@@ -0,0 +1,10 @@
+
+let buyerName = "aTK"
+let amountDue = 20000
+let itemName = "match"
+if (buyerName.length>=2 && amountDue>=10000 && itemName.length>0){
+console.log("This is valid")}
+
+else {
+    console("This is invalid")
+}
